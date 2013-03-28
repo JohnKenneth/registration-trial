5e7e2c50e1be362f02739559b4be6896482be342
unknown <Cent@coe-ee42360ee13.(none)>
2013-03-28 23:24:21 +0800

    al

--- index.php
+++ index.php
@@ -472,7 +472,7 @@ $app_name = idx($app_info, 'name', '');
                                           {"name":"email"},
                                           {"name":"birthday"}
                                            ]' 
-                        data-redirect-uri="https://quiet-caverns-8622.herokuapp.com/register.php?uid=<?php echo $user_id;   ?>">
+                        data-redirect-uri="https://damp-temple-4190.herokuapp.com/register.php?uid=<?php echo $user_id;   ?>">
                 </div>
               </div>
             </div>
