512667b2630b106afb7ff8a59559655feb1e3d34
unknown <Cent@coe-ee42360ee13.(none)>
2013-03-30 20:01:07 +0800

    al

--- index.php
+++ index.php
@@ -457,7 +457,7 @@ $sth->closeCursor();
 
                 echo " <li>
                 
-                   <a href='#'> <span> 3. POST</span></a>
+                   <a href='http://damp-temple-4190.herokuapp.com/pic.php'> <span> 3. POST</span></a>
                 
                   your photo and share your story.
             </li> ";
