e2ddab53161b96426876f370c7b52ed106dd1d0d
unknown <Cent@coe-ee42360ee13.(none)>
2013-03-31 20:20:08 +0800

    al

--- pic.php
+++ pic.php
@@ -69,29 +69,28 @@
  echo realpath($_FILES['source']['name']);
 
     $args = array(
-      'source' => '@'. $_FILES['source']['name'],
     'message' => $_POST['message']
     );
-  // $args[basename($file)] = '@' . realpath($file);
+   $args[basename($file)] = '@' . realpath($file);
     $url = 'https://graph.facebook.com/'.$user.'/photos?access_token='.$access_token;
-
-    $ch = curl_init();
+print_r($args);
+    // $ch = curl_init();
    
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $args);
-    $data = curl_exec($ch);
-     //  if($data)
-     //  {
-     //   header('location:https://apps.facebook.com/160936377399430/');
-     // }
-     // else
-     // {
-     //  echo "error";
-     // }
-    print_r(json_decode($data,true));
+    // curl_setopt($ch, CURLOPT_URL, $url);
+    // curl_setopt($ch, CURLOPT_HEADER, false);
+    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // curl_setopt($ch, CURLOPT_POST, true);
+    // curl_setopt($ch, CURLOPT_POSTFIELDS, $args);
+    // $data = curl_exec($ch);
+    //  //  if($data)
+    //  //  {
+    //  //   header('location:https://apps.facebook.com/160936377399430/');
+    //  // }
+    //  // else
+    //  // {
+    //  //  echo "error";
+    //  // }
+    // print_r(json_decode($data,true));
     
 ?>
 
