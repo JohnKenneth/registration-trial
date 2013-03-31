3b5af466ab7d0105e7bdf8dc653010bf62128081
unknown <Cent@coe-ee42360ee13.(none)>
2013-03-31 19:01:06 +0800

    al

--- pic.php
+++ pic.php
@@ -64,7 +64,7 @@
     echo $_POST['message'];
 
  echo "</br>";
- echo realpath($_POST['source']);
+ echo realpath($_FILES['source']);
 
  //    $args = array(
  //      'source' => '@'. realpath($_POST['source']),
