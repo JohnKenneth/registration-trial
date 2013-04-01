e799546b196c2a719ed79d436d8f72292f905620
unknown <Cent@coe-ee42360ee13.(none)>
2013-04-02 01:38:29 +0800

    al

--- pic.php
+++ pic.php
@@ -25,7 +25,7 @@
         $result=$res->fetchAll();
         echo "<div id='s2' style='position:relative;'>";
 
-       echo $resu['albumid'];
+      
         if($resu['albumid']!=null || $resu['albumid']!="")
         {
            $t2=curl_init();
