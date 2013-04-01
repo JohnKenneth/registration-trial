0677cac1922b03c64efd1de461ba852290d012cf
unknown <Cent@coe-ee42360ee13.(none)>
2013-04-01 20:39:02 +0800

    al

--- index.php
+++ index.php
@@ -497,11 +497,11 @@ if($limit<=3145728)
     curl_setopt($t, CURLOPT_RETURNTRANSFER, true);
      curl_setopt($t, CURLOPT_HTTPGET, true);
      $AL=curl_exec($t);
-     $out=json_decode($AL)
+     $out=json_decode($AL);
 
-     // print_r($out);
+    print_r($out);
 
-     echo count($out);
+      echo count($out."al");
   
     }
     else
