bedfa44bf6a9aa73cfe25e228f2e38030759c9da
unknown <Cent@coe-ee42360ee13.(none)>
2013-04-02 10:21:32 +0800

    al

--- index.php
+++ index.php
@@ -631,6 +631,7 @@ $sth->closeCursor();
                   echo "You and ";
                 echo ($result2-1);
                 echo " people are registered.";
+                echo $user_id;
                }
               }
               else
