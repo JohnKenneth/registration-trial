82ee8e2c41f6f8a6d6a919ba3166df101cac7b9b
unknown <Cent@coe-ee42360ee13.(none)>
2013-04-05 18:14:15 +0800

    al

--- index.php
+++ index.php
@@ -299,25 +299,25 @@ $('#s2').cycle({
 
     <header class="clearfix">
       <?php if (isset($basic)) { ?>
-<!--       <p id="picture" style="background-image: url(https://graph.facebook.com/<?php echo he($user_id); ?>/picture?type=normal)"></p>
+     <!--  <p id="picture" style="background-image: url(https://graph.facebook.com/<?php //echo he($user_id); ?>/picture?type=normal)"></p>
 hear
-      <div>
-        <!-- <h1>Welcome , <strong><?php echo he(idx($basic, 'name')); ?></strong></h1> -->
+      <div> -->
+        <!-- <h1>Welcome , <strong><?php //echo he(idx($basic, 'name')); ?></strong></h1>
         <p class="tagline">
           Application
-          <a href="<?php echo he(idx($app_info, 'link'));?>" target="_top"><?php echo he($app_name); ?></a>
+          <a href="<?php //echo he(idx($app_info, 'link'));?>" target="_top"><?php //echo he($app_name); ?></a>
         </p>
 
         <div id="share-app">
           <p>Share your app:</p>
           <ul>
             <li>
-              <a href="#" class="facebook-button" id="postToWall" data-url="<?php echo AppInfo::getUrl(); ?>">
+              <a href="#" class="facebook-button" id="postToWall" data-url="<?php //echo AppInfo::getUrl(); ?>">
                 <span class="plus">Post to Wall</span>
               </a>
             </li>
             <li>
-              <a href="#" class="facebook-button speech-bubble" id="sendToFriends" data-url="<?php echo AppInfo::getUrl(); ?>">
+              <a href="#" class="facebook-button speech-bubble" id="sendToFriends" data-url="<?php //echo AppInfo::getUrl(); ?>">
                 <span class="speech-bubble">Send Message</span>
               </a>
             </li>
