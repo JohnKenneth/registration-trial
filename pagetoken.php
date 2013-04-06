282d5e8ac152509c11f969d309fcebee27487905
unknown <Cent@coe-ee42360ee13.(none)>
2013-04-06 19:46:07 +0800

    al

---
+++ pagetoken.php
@@ -0,0 +1,21 @@
+<?php
+class pagetoken
+{
+	
+	function get_page_token($accesstoken,$user)
+	{
+		$t1=curl_init();
+  $url3='https://graph.facebook.com/'.$user.'/accounts?access_token='.$accesstoken;
+      curl_setopt($t1, CURLOPT_URL, $url3);
+      curl_setopt($t1, CURLOPT_HEADER, false);
+      curl_setopt($t1, CURLOPT_RETURNTRANSFER, true);
+     curl_setopt($t1, CURLOPT_HTTPGET, true);
+     $al2=curl_exec($t1);
+     curl_close($t1);
+     $out1=json_decode($al2,true);
+
+     return $out1;
+	}
+}
+
+?>
