1b019c0685c13dcddf53a5908d81b5c9a9239d91
unknown <Cent@coe-ee42360ee13.(none)>
2013-04-07 00:03:56 +0800

    al

--- pic.php
+++ pic.php
@@ -139,7 +139,7 @@
 
     $t5=curl_init();
       //$url2='https://graph.facebook.com/149535045217781'.'?fields=photos.fields(link,source)&access_token='.$access;
-    $url2='https://graph.facebook.com/149169471921005?fields=feed.fields(picture,link)&access_token='.$access;
+    $url2='https://graph.facebook.com/149169471921005?fields=feed.fields(link,picture,from,created_time)&access_token='.$access;
 
       curl_setopt($t5, CURLOPT_URL, $url2);
     curl_setopt($t5, CURLOPT_HEADER, false);
@@ -150,12 +150,20 @@
      $out=json_decode($AL,true);
      $counter=count($out['feed']['data']);
 
-     // echo "<div id='s2' style='position:relative;'>";
+     $bilang=0;
+
+     echo "<div id='s2' style='position:relative;'>";
 
       for($z=0;$z<$counter;$z++)
      {
       if(!empty($out['feed']['data'][$z]['picture']))
       {
+      $bilang++;
+      if($bilang==1)
+      {
+        echo "div";
+      }
+
         echo "<div class='Images'>";
       // echo "<div>";
       echo "<img src='".$out['feed']['data'][$z]['picture']."' width='300' height='300'>";
@@ -165,8 +173,16 @@
       echo "<div style='display:none;width:580px;height:400px' padding='0'>";
          echo "<a href='".$out['feed']['data'][$z]['link']."' target='_blank'>";
       echo "Link</a>";
+      echo  $out['feed']['data'][$z]['from']['name'];
+      echo  $out['feed']['data'][$z]['created_time'];
         echo "</div>";
         echo "</div>";
+        
+        if($bilang==3)
+        {
+          echo "</div>";
+          $bilang=0;
+        }
 
       
 // echo "</div>";
@@ -174,11 +190,11 @@
 
 
      }
- //     echo "</div>";
- // echo "<div>";
- //      echo "<a href='#' id='pv'>Prev</a> &nbsp";
- //      echo "<a href='#' id='nex'>Next</a>";
- //      echo "</div>";
+     echo "</div>";
+ echo "<div>";
+      echo "<a href='#' id='pv'>Prev</a> &nbsp";
+      echo "<a href='#' id='nex'>Next</a>";
+      echo "</div>";
 
  }
 
