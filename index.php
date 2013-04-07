b3c7c8a4a51a1a98aa1e2296ae97b3bcb2636174
unknown <Cent@coe-ee42360ee13.(none)>
2013-04-07 14:13:19 +0800

    al

--- index.php
+++ index.php
@@ -558,7 +558,7 @@ if($limit<=3145728)
 
 
     $args = array(
-      'image' => '@'.$file,
+      'source' => '@'.$file,
     'message' => $_POST['message']
     );
    //$args[basename($file)] = '@' . $file;
