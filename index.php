9ce28ce076941c68640b24286b03e918d7e1b8f5
unknown <Cent@coe-ee42360ee13.(none)>
2013-04-30 19:01:52 +0800

    al

--- index.php
+++ index.php
@@ -240,19 +240,19 @@ $('#s2').cycle({
     prev:   '#pv' 
       });
 
-    // $('#pheets').click(function()
-
-    //   {
-    //       top.location.href= 'https://www.facebook.com/TestRegistrationCommunity';
-
-    //   });
-
- $('#pheets').click(function()
+    $('#pheets').click(function()
 
       {
-         $('#phoots').toggle();
+          top.location.href= 'https://www.facebook.com/TestRegistrationCommunity';
 
       });
+
+ // $('#pheets').click(function()
+
+ //      {
+ //         $('#phoots').toggle();
+
+ //      });
 
 
         $('#sendRequest').click(function() {
@@ -815,7 +815,7 @@ $sth->closeCursor();
                 echo "</li>";
            
                
-              echo "<li>";
+              // echo "<li>";
 
              echo " <li>
                 
@@ -826,22 +826,22 @@ $sth->closeCursor();
 
            
            // $graph_url=$_SERVER['PHP_SELF'];
-               $graph_url='https://damp-temple-4190.herokuapp.com/index.php';
+         //       $graph_url='https://damp-temple-4190.herokuapp.com/index.php';
          
 
-         echo "<div id='phoots'>";
-         echo '<form  enctype="multipart/form-data" action="'
-         .$graph_url .' "method="POST">';
-         echo 'Please choose a photo: ';
-         echo '<input name="source" type="file"><br/><br/>';
-         echo 'Say something about this photo: ';
-         echo '<input name="message" 
-             type="text" value=""><br/><br/>';
-         echo '<input type="submit" value="Upload" name="submit"/><br/>';
-         echo '</form>';
-         echo '</div>';
+         // echo "<div id='phoots'>";
+         // echo '<form  enctype="multipart/form-data" action="'
+         // .$graph_url .' "method="POST">';
+         // echo 'Please choose a photo: ';
+         // echo '<input name="source" type="file"><br/><br/>';
+         // echo 'Say something about this photo: ';
+         // echo '<input name="message" 
+         //     type="text" value=""><br/><br/>';
+         // echo '<input type="submit" value="Upload" name="submit"/><br/>';
+         // echo '</form>';
+         // echo '</div>';
 
-          echo "</li>";
+         //  echo "</li>";
 
            $photos= new pic();
             $photos->pics_registered($facebook->getAccessToken(),$user_id);
