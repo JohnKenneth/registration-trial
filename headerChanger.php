607af2a6b3b8d84997a239c19b91044adb2c3c5d
unknown <Cent@coe-ee42360ee13.(none)>
2013-05-01 19:11:14 +0800

    alpogi

--- headerChanger.php
+++ headerChanger.php
@@ -1,6 +1,16 @@
 <?php
+require_once('config.php');
+require_once('lib/db.php');
 
 $link=$_POST["link"];
-mysql_query("update header set link='$link' WHERE id=1",$con);
-echo "<script type='text/javascript'>window.location='https://damp-temple-4190.herokuapp.com/index.php';</script>";
+//mysql_query("update header set link='$link' WHERE id=1",$con);
+
+
+$db = Db::init();
+         $admin=$db->prepare("update header set link='$link' WHERE id=1");
+         $admin->execute();
+         //echo "<script type='text/javascript'>window.location='https://damp-temple-4190.herokuapp.com/index.php';</script>";
+         echo "<script type='text/javascript'> top.location.href= 'https://www.facebook.com/TestRegistrationCommunity/app_160936377399430';</script>";
+        
+
 ?>
