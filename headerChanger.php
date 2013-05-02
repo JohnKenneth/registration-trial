661b041ed6738b4cc5e1404962f3a23c7e8ff1e5
John Kenneth Lorca <johnkennethlorca@yahoo.com>
2013-05-02 19:50:11 +0800

    kenneth

--- headerChanger.php
+++ headerChanger.php
@@ -7,10 +7,9 @@ $name=$_POST["name"];
 $link=$_POST["link"];
 $limit=$_POST["limit"];
 //mysql_query("update header set link='$link' WHERE id=1",$con);
-
+echo $add;
  if(!isset($_POST["link"]))
  {
-	echo "hi";
          //echo "<script type='text/javascript'>window.location='https://damp-temple-4190.herokuapp.com/index.php';</script>";
          // echo "<script type='text/javascript'> top.location.href= 'https://www.facebook.com/TestRegistrationCommunity/app_160936377399430';</script>";
          // $db=null;
@@ -25,7 +24,6 @@ $limit=$_POST["limit"];
          $admin=$db->prepare($param);
          $admin->execute();
  }
- echo "hello";
  if(!isset($_POST["limit"]))    
  {	
          //echo "<script type='text/javascript'>window.location='https://damp-temple-4190.herokuapp.com/index.php';</script>";
@@ -34,7 +32,7 @@ $limit=$_POST["limit"];
  }  
  else
  {
-  echo "hello";
+  echo $limit;
 	$param="update header set limit='$limit' WHERE id=1";
 	$db = Db::init();
          $admin=$db->prepare($param);
@@ -51,7 +49,7 @@ $limit=$_POST["limit"];
  }  
  else
  {
-  echo "hello1";
+  echo $add;
 	$param="INSERT INTO administrators VALUES(null,'$add','$name',null)";
 	$db = Db::init();
          $admin=$db->prepare($param);
