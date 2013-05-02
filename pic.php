a5292b52d020dc264d74adaa418d5620f19dbd1f
John Kenneth Lorca <johnkennethlorca@yahoo.com>
2013-05-02 21:05:16 +0800

    kenneth

--- pic.php
+++ pic.php
@@ -229,7 +229,7 @@ echo "<table><tr>";
 
        echo "<center>";
        echo "</br>";
-      echo "<div style='padding-right:150px;padding-top:10px'>";
+      echo "<div style='padding-right:150px;padding-top:10px;font:BOLD 15px;'>";
       echo "<a href='#' id='pv'>Prev</a> &nbsp;&nbsp;&nbsp;";
       echo "<a href='#' id='nex'>Next</a>";
       echo "</div>";
