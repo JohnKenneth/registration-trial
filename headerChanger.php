5b7cce96765079ea3dd9c1954a4019f49fd46834
unknown <Cent@coe-ee42360ee13.(none)>
2013-05-02 15:51:13 +0800

    alpogi

--- headerChanger.php
+++ headerChanger.php
@@ -18,14 +18,15 @@ if(isset($link))
 
  if(isset($limitno))    
  {	
- 	echo $limitno;
+ 	
  	$db1 = Db::init();
          $admin1=$db1->prepare("update header set limit=$limitno WHERE id=1");
          $admin1->execute();
          
          //echo "<script type='text/javascript'>window.location='https://damp-temple-4190.herokuapp.com/index.php';</script>";
 // echo "<script type='text/javascript'>  top.location.href= 'https://www.facebook.com/TestRegistrationCommunity/app_160936377399430';</script>";
-          header( "Location:https://www.facebook.com/TestRegistrationCommunity/app_160936377399430");
+         
  }   
+  header( "Location:https://www.facebook.com/TestRegistrationCommunity/app_160936377399430");
 
 ?>
