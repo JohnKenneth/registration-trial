eca5805b62f7c9750d42567a7b01f624a9c1a110
John Kenneth Lorca <johnkennethlorca@yahoo.com>
2013-05-02 20:04:09 +0800

    kenneth

--- headerChanger.php
+++ headerChanger.php
@@ -8,8 +8,9 @@ $link=$_POST["link"];
 $limit=$_POST["limit"];
 //mysql_query("update header set link='$link' WHERE id=1",$con);
 echo $add;
- if(!isset($_POST["link"]))
+ if(isset($_POST["link"]))
  {
+	$param="update header set link='$link' WHERE id=1";
          //echo "<script type='text/javascript'>window.location='https://damp-temple-4190.herokuapp.com/index.php';</script>";
          // echo "<script type='text/javascript'> top.location.href= 'https://www.facebook.com/TestRegistrationCommunity/app_160936377399430';</script>";
          // $db=null;
@@ -17,33 +18,20 @@ echo $add;
          // $admin1=$db1->prepare("update header set limit=5 WHERE id=1");
          // $admin1->execute();
  }
- else
- {
-	$param="update header set link='$link' WHERE id=1";
- }
- if(!isset($_POST["limit"]))    
+ elseif(isset($_POST["limit"]))    
  {	
-         //echo "<script type='text/javascript'>window.location='https://damp-temple-4190.herokuapp.com/index.php';</script>";
-// echo "<script type='text/javascript'>  top.location.href= 'https://www.facebook.com/TestRegistrationCommunity/app_160936377399430';</script>";
-         
- }  
- else
- {
 	$param1="update `header` set `limit`=$limit WHERE id=1";
- } 
- if(!isset($_POST["add"]))    
- {	
- 	
- 	
-         
          //echo "<script type='text/javascript'>window.location='https://damp-temple-4190.herokuapp.com/index.php';</script>";
 // echo "<script type='text/javascript'>  top.location.href= 'https://www.facebook.com/TestRegistrationCommunity/app_160936377399430';</script>";
          
  }  
- else
- {
-	$param2="INSERT INTO `administrators` VALUES(null,'$add','$name','')";
+ elseif(isset($_POST["add"]))    
+ {	
+ 	$param2="INSERT INTO `administrators` VALUES(null,'$add','$name','')";
  }
+         
+         //echo "<script type='text/javascript'>window.location='https://damp-temple-4190.herokuapp.com/index.php';</script>";
+// echo "<script type='text/javascript'>  top.location.href= 'https://www.facebook.com/TestRegistrationCommunity/app_160936377399430';</script>";
  
 	$db = Db::init();
          $admin=$db->prepare($param2);
