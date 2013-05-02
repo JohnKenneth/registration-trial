a33c5a1b81d6b503cb0d90280915712e56b14d11
unknown <Cent@coe-ee42360ee13.(none)>
2013-05-02 16:10:14 +0800

    alpogi

--- headerChanger.php
+++ headerChanger.php
@@ -14,6 +14,7 @@ if(isset($link))
          $admin->execute();
          //echo "<script type='text/javascript'>window.location='https://damp-temple-4190.herokuapp.com/index.php';</script>";
          echo "<script type='text/javascript'> top.location.href= 'https://www.facebook.com/TestRegistrationCommunity/app_160936377399430';</script>";
+         $db=null;
  }
 
  if(!empty($limitno))    
