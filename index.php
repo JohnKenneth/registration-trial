12af16121510b208399e9b8767c3c059463242ff
John Kenneth Lorca <johnkennethlorca@yahoo.com>
2013-05-02 19:17:23 +0800

    kenneth

--- index.php
+++ index.php
@@ -299,7 +299,7 @@ $app_name = idx($app_info, 'name', '');
 		temp=document.getElementById("add").value;
 		myInput = document.createElement("input") ;
 		myInput.setAttribute("name", "name");
-		var name=$("#add").text();
+		var name=$("#add").text($(this).find(":selected").text());
 		alert(name);
 		myInput.setAttribute("value", name);
 		myForm.appendChild(myInput) ;
