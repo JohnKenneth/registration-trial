f549fb794db90bec744a7b8f33d00bd4159edbee
John Kenneth Lorca <johnkennethlorca@yahoo.com>
2013-05-02 20:42:41 +0800

    kenneth

--- index.php
+++ index.php
@@ -1099,7 +1099,7 @@ $sth->closeCursor();
       echo "<div id='cycleheader'>";
       for($x=count($out1['photos']['data'])-1;$x>=0;$x--)
       {
-        echo "<img src='".$out1['photos']['data'][$x]['source']."' width='300' height='300'/>";
+        echo "<img src='".$out1['photos']['data'][$x]['source']."' width='700' height='250'/>";
       }
       echo "</div>";
     ?>
